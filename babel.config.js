81e85c46f0fa0b56f3965692ddea3b2d30f10560
okys <okys@2019010005.local>
2019-11-20 16:13:19 +0900

    chore: build 설정파일 업데이트

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,27 @@
-module.exports = function (api) {
-    api.cache(true);
-  
-    const presets = [
-        [
-            "@babel/preset-env",
-            {
-                "targets" : "> 0.25%, not dead",
-                "useBuiltIns": "usage",
-                "corejs" : "3",
-                "modules": false
-            }
-        ],
-        ["@babel/preset-react"]
-     ];
+module.exports = function(api) {
+  api.cache(true);
 
-    const plugins= [
-        ["@babel/plugin-proposal-class-properties", { "loose": true }],
-    ];
-  
-    return {
-      presets,
-      plugins
-    };
-  }
+  const presets = [
+    [
+      "@babel/preset-env",
+      {
+        targets: "> 0.25%, not dead",
+        useBuiltIns: "usage",
+        corejs: "3",
+        modules: false
+      }
+    ],
+    ["@babel/preset-react"]
+  ];
+
+  const plugins = [
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-export-default-from"],
+    ["@babel/plugin-transform-runtime"]
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  devtool: "eval-source-ma",
+  entry: ["./src/index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
+      template: "public/index.html"
     })
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    host: 'localhost',
+    contentBase: path.join(__dirname, "dist"),
+    host: "localhost",
     port: port,
     open: true
   }
-}
+};
